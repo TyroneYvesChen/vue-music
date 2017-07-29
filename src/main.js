e6c5760b448a1efea83ed7fbb361ca07cdb8b15b
TyroneYvesChen <243307272@qq.com>
2017-07-29 15:23:48 +0800

    new package.json

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'
 
-Vue.config.productionTip = false
+import 'common/stylus/index.styl'
+
+fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  render: h => h(App)
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Recommend from 'components/recommend/recommend'
+import Search from 'components/search/search'
+import Singer from 'components/singer/singer'
+import Rank from 'components/rank/rank'
 
 Vue.use(Router)
 
@@ -8,8 +11,27 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      redirect: '/Recommend'
+    },
+    {
+      path: '/Recommend',
+      name: 'Recommend',
+      component: Recommend
+    },
+    {
+      path: '/Singer',
+      name: 'Singer',
+      component: Singer
+    },
+    {
+      path: '/Rank',
+      name: 'Rank',
+      component: Rank
+    },
+    {
+      path: '/Search',
+      name: 'Search',
+      component: Search
     }
   ]
 })
