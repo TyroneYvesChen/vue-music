b6b0f647b699e30cf099b9d230effcdb70de09c0
CZY_1992 <243307272@qq.com>
2017-07-31 14:48:42 +0800

    recommend

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,5 +1,14 @@
 import jsop from "common/js/jsonp"
+import {commonParams, options} from "common/js/config"
 
 export  function gitRecommend() {
-  const url = ""
+  const url = "https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg"
+
+  const data = Object.assign({}, commonParams, {
+    platform: 'h5',
+    uin: 0,
+    needNewCode: 1
+  })
+
+  return jsop(url, data, options)
 }
